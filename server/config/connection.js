b8edb37653f71f161a4369842e42e62a1c00bfd9
jesses <jswan7692@gmail.com>
2025-01-31 20:21:34 -0700

    final additions

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,17 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://jswan7692:mongodb7@cluster0.o8ap0.mongodb.net/googlebooks?retryWrites=true&w=majority&appName=Cluster0' || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://jswan7692:mongodb7@cluster0.o8ap0.mongodb.net/googlebooks?retryWrites=true&w=majority&appName=Cluster0' || 'mongodb://127.0.0.1:27017/googlebooks', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true,  // Force SSL
+    sslValidate: false,  // Disable certificate validation (only if needed)
+  })
+    .then(() => {
+      console.log('MongoDB connected');
+    })
+    .catch((err) => {
+      console.error('Error connecting to MongoDB:', err);
+    });
 
 module.exports = mongoose.connection;
