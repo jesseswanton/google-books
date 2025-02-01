398eb93afb53432151c0c23293944cf52f496afe
jesses <jswan7692@gmail.com>
2025-01-31 20:12:31 -0700

    final additions

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://jswan7692:mongodb7@cluster0.o8ap0.mongodb.net/googlebooks?retryWrites=true&w=majority&appName=Cluster0' || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
